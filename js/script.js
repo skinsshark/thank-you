829faaa64c3a684eabfdd53a9aa8b2d77b2b9a3a
SHARON ZHENG <sharzheng@gmail.com>
2019-03-14 11:56:41 -0400

    cube

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 anime({
-  targets: ['.outlined path', '.outlined polygon'],
+  targets: ['.outlined patha', '.outlined polygona'],
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
   duration: 2500,
@@ -8,3 +8,50 @@ anime({
   },
   direction: 'normal'
 });
+
+const faces = document.getElementsByTagName('svg');
+const cube = document.getElementById('cube');
+console.log(faces, cube)
+
+
+
+setTimeout(function(){
+  cube.classList.add('show-back');
+  console.log(cube);
+}, 10);
+
+setTimeout(function(){
+  cube.classList.remove('show-back');
+  cube.classList.add('show-left');
+  console.log(cube);
+}, 2010);
+
+setTimeout(function(){
+  cube.classList.remove('show-left');
+  cube.classList.add('show-top');
+  console.log(cube);
+}, 4010);
+
+setTimeout(function(){
+  cube.classList.remove('show-top');
+  cube.classList.add('show-right');
+  console.log(cube);
+}, 6010);
+
+setTimeout(function(){
+  cube.classList.remove('show-right');
+  cube.classList.add('show-bottom');
+  console.log(cube);
+}, 8010);
+
+setTimeout(function(){
+  cube.classList.remove('show-bottom');
+  cube.classList.add('show-left');
+  console.log(cube);
+}, 10010);
+
+setTimeout(function(){
+  cube.classList.remove('show-left');
+  cube.classList.add('show-back');
+  console.log(cube);
+}, 12010);
