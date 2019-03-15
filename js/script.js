ee9d0bcbe50eb29c0592cf257502c860b5d491e9
SHARON ZHENG <sharzheng@gmail.com>
2019-03-14 21:05:27 -0400

    add onclick events

--- js/script.js
+++ js/script.js
@@ -1,57 +1,49 @@
-anime({
-  targets: ['.outlined patha', '.outlined polygona'],
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 2500,
-  delay: function(el, i, l) {
-    return i * 50;
-  },
-  direction: 'normal'
-});
+// anime({
+//   targets: ['path', 'polygon'],
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   duration: 2500,
+//   delay: function(el, i, l) {
+//     return i * 50;
+//   },
+//   direction: 'normal'
+// });
 
 const faces = document.getElementsByTagName('svg');
 const cube = document.getElementById('cube');
-console.log(faces, cube)
+
+const SIDES = [
+  'front',
+  'back',
+  'right',
+  'left',
+  'top',
+  'bottom'
+];
+const numOfSides = SIDES.length;
+const seenFaces = new Set();
+
+function switchFace() {
+  cube.className = '';
+  cube.classList.add(`show-${randoSide()}`);
+}
+
+function randoSide() {
+  if (seenFaces.size === numOfSides) {
+    seenFaces.clear();
+  }
+
+  let startLength = seenFaces.size;
+  let res, indx;
+
+  do {
+    indx = Math.floor(
+      Math.random() * Math.floor(numOfSides)
+    );
+    res = SIDES[indx];
+    seenFaces.add(res);
+  } while (startLength === seenFaces.size);
 
 
-
-setTimeout(function(){
-  cube.classList.add('show-back');
-  console.log(cube);
-}, 10);
-
-setTimeout(function(){
-  cube.classList.remove('show-back');
-  cube.classList.add('show-left');
-  console.log(cube);
-}, 2010);
-
-setTimeout(function(){
-  cube.classList.remove('show-left');
-  cube.classList.add('show-top');
-  console.log(cube);
-}, 4010);
-
-setTimeout(function(){
-  cube.classList.remove('show-top');
-  cube.classList.add('show-right');
-  console.log(cube);
-}, 6010);
-
-setTimeout(function(){
-  cube.classList.remove('show-right');
-  cube.classList.add('show-bottom');
-  console.log(cube);
-}, 8010);
-
-setTimeout(function(){
-  cube.classList.remove('show-bottom');
-  cube.classList.add('show-left');
-  console.log(cube);
-}, 10010);
-
-setTimeout(function(){
-  cube.classList.remove('show-left');
-  cube.classList.add('show-back');
-  console.log(cube);
-}, 12010);
+  return res;
+}
